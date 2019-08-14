439a66224ad94c079179582864f309bbd54295bb
unknown <asambaevadele@gmail.com>
2019-08-14 16:11:31 +0600

    third commit

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-function my FunctiOn(){
+function myFunctiOn(){
     document. getElementById('demo').innerHTML = 'My first Web Page';
+}
+function heading(){
+    document.getElementById("head1").style.color="green";
+}
+
+function background(){
+    document.getElementById("bodyBack").style.backgroundColor="yellow";
+}
+function textFormat(){
+    document.getElementById("txt").style.fontSize = "50px";
+    document.getElementById("txt").style.fontFamily = "cursive";
 }
