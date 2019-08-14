a52f282fb8057be2f21e1f8fba8d9f22bc2474b5
unknown <asambaevadele@gmail.com>
2019-08-14 17:02:59 +0600

    forth commit

--- script.js
+++ script.js
@@ -3,12 +3,20 @@ function myFunctiOn(){
 }
 function heading(){
     document.getElementById("head1").style.color="green";
+    document.getElementById("head1").style.display="block"
 }
 
 function background(){
     document.getElementById("bodyBack").style.backgroundColor="yellow";
+    document.getElementById("head1").style.display="none";
 }
 function textFormat(){
     document.getElementById("txt").style.fontSize = "50px";
     document.getElementById("txt").style.fontFamily = "cursive";
+}
+function alertex(){
+    window.alert("Hello, it`s me - Alert!!!");
+}
+function consoleLog(){
+    console.log("Hello, Console");
 }
